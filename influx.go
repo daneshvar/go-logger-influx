b97539e312bb14b14ce098ca6ac312aa6361ff15
Hossein Daneshvar <daneshvar.ho@gmail.com>
2021-08-28 04:49:40 +0430

    added support to influxdb2

--- influx.go
+++ influx.go
@@ -1,10 +1,9 @@
 package logger
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/daneshvar/go-logger"
@@ -15,7 +14,7 @@ import (
 const measurement = "logs"
 
 type Influx struct {
-	pool     sync.Pool
+	// pool     sync.Pool
 	client   influxdb.Client
 	writeAPI api.WriteAPI
 	app      string
@@ -23,11 +22,11 @@ type Influx struct {
 
 func InfluxWriter(serverURL string, authToken string, org string, bucket string, app string, caller bool, stack logger.EnablerFunc, enabler logger.EnablerFunc) *logger.Writer {
 	encoder := &Influx{
-		pool: sync.Pool{New: func() interface{} {
-			b := bytes.NewBuffer(make([]byte, 150)) // buffer init with 150 size
-			b.Reset()
-			return b
-		}},
+		// pool: sync.Pool{New: func() interface{} {
+		// 	b := bytes.NewBuffer(make([]byte, 150)) // buffer init with 150 size
+		// 	b.Reset()
+		// 	return b
+		// }},
 		app: app,
 	}
 	encoder.Connect(serverURL, authToken, org, bucket)
@@ -47,19 +46,19 @@ func (i *Influx) Close() {
 	i.client.Close()
 }
 
-func (i *Influx) getBuffer() *bytes.Buffer {
-	return i.pool.Get().(*bytes.Buffer)
-}
+// func (i *Influx) getBuffer() *bytes.Buffer {
+// 	return i.pool.Get().(*bytes.Buffer)
+// }
 
-func (i *Influx) putBuffer(b *bytes.Buffer) {
-	b.Reset()
-	i.pool.Put(b)
-}
+// func (i *Influx) putBuffer(b *bytes.Buffer) {
+// 	b.Reset()
+// 	i.pool.Put(b)
+// }
 
 func (i *Influx) Print(l logger.Level, s string, caller string, stack []string, message []interface{}) {
 	fields := make(map[string]interface{})
 
-	fields["message"] = message
+	fields["message"] = fmt.Sprint(message...)
 	if caller != "" {
 		fields["caller"] = caller
 	}
@@ -97,6 +96,37 @@ func (i *Influx) Printv(l logger.Level, s string, caller string, stack []string,
 
 	i.addKeyValues(fields, keysValues)
 
+	jsonString, _ := json.Marshal(fields)
+
+	// create point
+	p := influxdb.NewPoint(
+		measurement,
+		map[string]string{
+			"app":   i.app,
+			"scope": s,
+			"level": logger.LevelText(l),
+		},
+		map[string]interface{}{
+			"values": jsonString,
+		},
+		time.Now())
+
+	// write asynchronously
+	i.writeAPI.WritePoint(p)
+}
+
+func (i *Influx) Prints(l logger.Level, s string, caller string, stack []string, message string) {
+	fields := make(map[string]interface{})
+
+	fields["message"] = message
+	if caller != "" {
+		fields["caller"] = caller
+	}
+
+	if len(stack) > 0 {
+		fields["stack"] = strings.Join(stack, "\r\n")
+	}
+
 	// create point
 	p := influxdb.NewPoint(
 		measurement,
@@ -112,24 +142,21 @@ func (i *Influx) Printv(l logger.Level, s string, caller string, stack []string,
 	i.writeAPI.WritePoint(p)
 }
 
-func (i *Influx) Prints(l logger.Level, s string, caller string, stack []string, message string) {
-}
-
 func (i *Influx) Printf(l logger.Level, s string, caller string, stack []string, format string, args []interface{}) {
+	i.Prints(l, s, caller, stack, fmt.Sprintf(format, args...))
 }
 
 func (i *Influx) addKeyValues(fields map[string]interface{}, keysValues []interface{}) {
 	lenValues := len(keysValues)
-	if lenValues > 1 {
-		values := i.getBuffer()
-		defer i.putBuffer(values)
+	if lenValues < 1 {
+		return
+	}
 
-		// first key=value
-		values.WriteString(fmt.Sprintf("%v=%v", keysValues[0], keysValues[1]))
-		for i := 2; i < lenValues; i += 2 {
-			values.WriteString(fmt.Sprintf("\r\n%v=%v", keysValues[i], keysValues[i+1]))
+	for i := 0; i < lenValues; i += 2 {
+		if i+1 < lenValues {
+			fields[fmt.Sprintf("%v", keysValues[i])] = keysValues[i+1]
+		} else {
+			fields[fmt.Sprintf("%v", keysValues[i])] = "!VALUE"
 		}
-
-		fields["values"] = values.Bytes()
 	}
 }
